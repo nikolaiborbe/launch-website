675c5e3d25169944829ef2b992ca1d2b08ae3032
Nikolai <nikolaiborbe@gmail.com>
2025-06-10 00:04:31 +0200

    fix background color

--- src/lib/components/TempWidget.svelte
+++ src/lib/components/TempWidget.svelte
@@ -2,7 +2,7 @@
   let { temp = 0 } = $props<{ temp: number }>();
 </script>
 
-<div class="p-4 rounded-xl border-2 border-slate-100 shadow-lg h-full ">
+<div class="p-4 rounded-xl border-2 bg-white border-slate-100 shadow-lg h-full ">
   <div class="temp-widget  h-full">
     <div class="temp-value text-4xl font-bold flex  h-full items-center justify-center  ">
       {temp}°C
